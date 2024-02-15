34988de0c629d5269ad45ac90f8cb37caa63a5f5
William H <william.hermozo@protonmail.com>
2024-02-15 23:10:33 +0100

    rename

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Content } from "@/components/Content";
+import { Content } from "@/components/Contentt";
 import { Disciplines } from "@/components/disciplines";
 import { People } from "@/components/people";
 
