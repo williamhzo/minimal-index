eb7af5351a12e8badb7d69e748586bcf6761ea45
William H <william.hermozo@protonmail.com>
2024-02-15 22:59:51 +0100

    add raw data

---
+++ data.ts
@@ -0,0 +1,31 @@
+export const artist = {
+  "Donald Judd": {
+    name: "Donald Judd",
+    bio: "Pivotal Minimalist artist, revolutionized art with his stark, geometric sculptures that redefine space and form.",
+    born: "American, 1928—1994",
+    links: {
+      MoMa: "http://localhost:3000/",
+      Artsy: "http://localhost:3000/",
+      Wikipedia: "http://localhost:3000/",
+    },
+    quote: "Design has to work. \nArt does not.",
+  },
+  "Ellsworth Kelly": {},
+  "Sol LeWitt": {},
+};
+
+export const architect = {
+  "Álvaro Siza Vieira": {},
+  "Ludwig Mies van der Rohe": {},
+};
+
+export const designer = {
+  "Dieter Rams": {},
+  "Hans J. Wegner": {},
+};
+
+export const data = {
+  artist,
+  architect,
+  designer,
+};
