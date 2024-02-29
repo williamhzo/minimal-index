d04f723b8c9ce8969db54c218724bf4fa37d2ede
William H <william.hermozo@protonmail.com>
2024-02-29 20:08:11 +0100

    add suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import { Content } from "@/components/content";
 import { Disciplines } from "@/components/disciplines";
 import { People } from "@/components/people";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="flex gap-6 overflow-x-auto">
       <Disciplines />
-      <People />
-      <Content />
+
+      <Suspense>
+        <People />
+      </Suspense>
+
+      <Suspense>
+        <Content />
+      </Suspense>
     </div>
   );
 }
