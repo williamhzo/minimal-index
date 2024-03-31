70fe17f977598647dbda7d6009830ee0f43a804e
William H <william.hermozo@protonmail.com>
2024-03-31 15:08:30 +0200

    moar content

--- data.ts
+++ data.ts
@@ -43,6 +43,8 @@ type Discipline = {
   name: string;
 };
 
+const PI_RATIO = 291 / 181;
+
 export const disciplines: Discipline[] = [
   { id: "artist", name: "Artist" },
   { id: "architect", name: "Architect" },
@@ -84,7 +86,7 @@ export const personalities: Array<Personality> = [
             alt: "",
             url: "https://picsum.photos/id/35/640/362",
           },
-          aspectRatio: 291 / 181,
+          aspectRatio: PI_RATIO,
         },
         {
           id: "4",
@@ -98,7 +100,7 @@ export const personalities: Array<Personality> = [
             alt: "",
             url: "https://picsum.photos/id/13/640/362",
           },
-          aspectRatio: 291 / 181,
+          aspectRatio: PI_RATIO,
         },
         {
           id: "6",
@@ -106,7 +108,7 @@ export const personalities: Array<Personality> = [
             alt: "",
             url: "https://picsum.photos/id/87/640/362",
           },
-          aspectRatio: 291 / 181,
+          aspectRatio: PI_RATIO,
         },
         {
           id: "7",
@@ -160,7 +162,7 @@ export const personalities: Array<Personality> = [
             alt: "",
             url: "https://picsum.photos/id/9/640/362",
           },
-          aspectRatio: 291 / 181,
+          aspectRatio: PI_RATIO,
         },
         {
           id: "14",
@@ -192,34 +194,141 @@ export const personalities: Array<Personality> = [
     name: "Ellsworth Kelly",
     bio: {
       image: "https://picsum.photos/id/10/640/362",
-      description: "todo",
-      born: "todo",
+      description:
+        "A key figure in Minimalism, celebrated for his bold, monochromatic canvases that distill visual experience into pure color and shape.",
+      born: "American, 1923—2015",
     },
     ressources: [{ label: "", subLabel: "", link: "" }],
-    quote: "todo",
+    quote: "The negative is just as important as the positive",
     projects: {
       row1: [
         {
           id: "1",
-          title: "todo",
-          description: "todo",
           image: {
             url: "https://picsum.photos/id/11/640/362",
             alt: "",
           },
-          aspectRatio: 1,
+          aspectRatio: 16 / 9,
+        },
+        {
+          id: "2",
+          image: {
+            url: "https://picsum.photos/id/11/640/362",
+            alt: "",
+          },
+          aspectRatio: 16 / 9,
+        },
+        {
+          id: "3",
+          title: "Ellsworth Kelly on Abstraction",
+          description: "2019",
+          video: "https://todo.com/",
+          aspectRatio: 4 / 3,
+        },
+        {
+          id: "4",
+          image: {
+            url: "https://picsum.photos/id/43/640/362",
+            alt: "",
+          },
+          aspectRatio: 16 / 9,
+        },
+        {
+          id: "5",
+          description:
+            "Kelly applied his minimalist ethos with a keen eye for balance and proportion, often employing monochromatic schemes and simple shapes that harmonize with their surroundings, reflecting a deep understanding of the spatial and contextual relationship between art and its environment.",
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "6",
+          image: {
+            url: "https://picsum.photos/id/34/640/362",
+            alt: "",
+          },
+          aspectRatio: 16 / 9,
+        },
+        {
+          id: "7",
+          links: [
+            {
+              label: "Lorem Ipsum",
+              subLabel: "TBC",
+              url: "https://todo.com/",
+            },
+            {
+              label: "Lorem Ipsum",
+              subLabel: "TBC",
+              url: "https://todo.com/",
+            },
+            {
+              label: "Lorem Ipsum",
+              subLabel: "TBC",
+              url: "https://todo.com/",
+            },
+          ],
+          aspectRatio: 1 / 1,
         },
       ],
       row2: [
         {
-          id: "2",
-          title: "todo",
-          description: "todo",
+          id: "8",
           image: {
             url: "https://picsum.photos/id/12/640/362",
             alt: "",
           },
-          aspectRatio: 1,
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "9",
+          description:
+            "Kelly's art is a masterful exploration of color and form, stripping away the superfluous to focus on the essence of visual experience, resulting in bold, abstract works that challenge the viewer's perception and evoke a sense of pure emotion and simplicity.",
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "10",
+          image: {
+            url: "https://picsum.photos/id/12/640/362",
+            alt: "",
+          },
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "11",
+          image: {
+            url: "https://picsum.photos/id/12/640/362",
+            alt: "",
+          },
+          aspectRatio: PI_RATIO,
+        },
+        {
+          id: "12",
+          image: {
+            url: "https://picsum.photos/id/12/640/362",
+            alt: "",
+          },
+          aspectRatio: PI_RATIO,
+        },
+        {
+          id: "13",
+          image: {
+            url: "https://picsum.photos/id/12/640/362",
+            alt: "",
+          },
+          aspectRatio: PI_RATIO,
+        },
+        {
+          id: "14",
+          description:
+            "Kelly's manipulation of space is characterized by his ability to transform environments through the interplay of light, color, and form, creating dynamic visual experiences that engage the viewer in a dialogue with the physical space, making his works not just objects of contemplation but integral components of the architectural landscape.",
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "15",
+          image: {
+            url: "https://picsum.photos/id/12/640/362",
+            alt: "",
+          },
+          aspectRatio: PI_RATIO,
         },
       ],
     },
@@ -344,34 +453,131 @@ export const personalities: Array<Personality> = [
     name: "Hans J. Wegner",
     bio: {
       image: "https://picsum.photos/id/22/640/362",
-      description: "todo",
-      born: "todo",
+      description:
+        "Pioneer of Danish minimalist style and wood furniture - simply one of Scandinavia's most admired designers.",
+      born: "Danish, 1914—2007",
     },
     ressources: [{ label: "", subLabel: "", link: "" }],
-    quote: "todo",
+    quote:
+      "A chair is to have no backside. It should be beautiful from all sides and angles.",
     projects: {
       row1: [
         {
           id: "1",
-          title: "todo",
-          description: "todo",
           image: {
             url: "https://picsum.photos/id/23/640/362",
             alt: "",
           },
-          aspectRatio: 1,
+          aspectRatio: 16 / 9,
+        },
+        {
+          id: "2",
+          description:
+            "Wegner, also known as the 'Master of the Chair', designed more than 500 chairs during his career — many of which are considered masterpieces.",
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "3",
+          image: {
+            url: "https://picsum.photos/id/23/640/362",
+            alt: "",
+          },
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "4",
+          image: {
+            url: "https://picsum.photos/id/23/640/362",
+            alt: "",
+          },
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "5",
+          image: {
+            url: "https://picsum.photos/id/23/640/362",
+            alt: "",
+          },
+          aspectRatio: 4 / 3,
+        },
+        {
+          id: "6",
+          title: "Functional Beauty",
+          description:
+            "His designs were deeply rooted in the belief that beauty arises from utility, embodying a seamless blend of form and function.",
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "7",
+          image: {
+            url: "https://picsum.photos/id/23/640/362",
+            alt: "",
+          },
+          aspectRatio: 16 / 9,
         },
       ],
       row2: [
         {
-          id: "2",
-          title: "todo",
-          description: "todo",
+          id: "8",
+          title: "Craft Over Mass Production",
+          description:
+            "Wegner prioritized traditional craftsmanship techniques, ensuring each piece of furniture was not only aesthetically pleasing but also exceptionally well-made.",
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "9",
           image: {
-            url: "https://picsum.photos/id/24/640/362",
+            url: "https://picsum.photos/id/23/640/362",
             alt: "",
           },
-          aspectRatio: 1,
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "10",
+          image: {
+            url: "https://picsum.photos/id/23/640/362",
+            alt: "",
+          },
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "11",
+          image: {
+            url: "https://picsum.photos/id/23/640/362",
+            alt: "",
+          },
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "12",
+          title: "Ergonomic Focus",
+          description:
+            "Wegner's furniture is celebrated for its comfort and ergonomics, designed to support the human body naturally and elegantly.",
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "13",
+          image: {
+            url: "https://picsum.photos/id/23/640/362",
+            alt: "",
+          },
+          aspectRatio: 4 / 3,
+        },
+        {
+          id: "14",
+          image: {
+            url: "https://picsum.photos/id/23/640/362",
+            alt: "",
+          },
+          aspectRatio: 1 / 1,
+        },
+        {
+          id: "15",
+          image: {
+            url: "https://picsum.photos/id/23/640/362",
+            alt: "",
+          },
+          aspectRatio: 1 / 1,
         },
       ],
     },
