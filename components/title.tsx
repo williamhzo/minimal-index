71ea618989ee91686f77a7894494079e378914fe
William H <william.hermozo@protonmail.com>
2024-03-31 15:21:33 +0200

    link in title

--- components/title.tsx
+++ components/title.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils";
+import Link from "next/link";
 import { Fragment } from "react";
 
 export const Title = () => {
@@ -6,25 +7,27 @@ export const Title = () => {
   const splitTitle = title.split(" ").map((word) => word.split(""));
 
   return (
-    <h1 className="group flex select-none gap-8 text-lg uppercase">
-      {splitTitle.map((word, wordIndex) => (
-        <span key={wordIndex} className="flex gap-3">
-          {word.map((letter, letterIndex) => (
-            <span
-              key={letterIndex}
-              className={cn(
-                "transition-colors duration-300",
-                letterIndex === 0
-                  ? "text-content"
-                  : "text-background group-hover:text-content",
-              )}
-            >
-              {letter}
-            </span>
-          ))}
-          {wordIndex < splitTitle.length - 1 ? " " : ""}
-        </span>
-      ))}
+    <h1 className="flex h-[--title-height] items-center">
+      <Link href="/" className="group flex select-none gap-8 text-lg uppercase">
+        {splitTitle.map((word, wordIndex) => (
+          <span key={wordIndex} className="flex gap-3">
+            {word.map((letter, letterIndex) => (
+              <span
+                key={letterIndex}
+                className={cn(
+                  "transition-colors duration-300",
+                  letterIndex === 0
+                    ? "text-content"
+                    : "text-background group-hover:text-content",
+                )}
+              >
+                {letter}
+              </span>
+            ))}
+            {wordIndex < splitTitle.length - 1 ? " " : ""}
+          </span>
+        ))}
+      </Link>
     </h1>
   );
 };
