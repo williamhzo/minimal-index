f985b94e12bd5208a8ccf8fa23537c19d5f5456d
William H <william.hermozo@protonmail.com>
2024-06-04 21:51:59 +0200

    rm million

--- components/home-content.tsx
+++ components/home-content.tsx
@@ -3,29 +3,10 @@
 import { Content } from "@/components/content";
 import { Disciplines } from "@/components/disciplines";
 import { People } from "@/components/people";
-import { useRef, useEffect } from "react";
 
 export const HomeContent = () => {
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleScroll = (event: WheelEvent) => {
-      console.log(event.deltaY);
-      if (Math.abs(event.deltaY) > 0 && scrollContainerRef.current) {
-        event.preventDefault();
-        scrollContainerRef.current.scrollLeft += event.deltaY;
-      }
-    };
-
-    window.addEventListener("wheel", handleScroll, { passive: false });
-
-    return () => {
-      window.removeEventListener("wheel", handleScroll);
-    };
-  }, []);
-
   return (
-    <div ref={scrollContainerRef} className="mr-12 flex h-full w-full gap-0">
+    <div className="mr-12 flex h-full w-full gap-0">
       <Disciplines />
 
       <div className="flex gap-6">
