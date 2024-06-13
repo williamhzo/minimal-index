0eef6c0cff8e06d2a33b11ce6622d2d41c785286
William H <william.hermozo@protonmail.com>
2024-06-13 18:43:43 +0200

    extract createQueryString

--- utils.ts
+++ utils.ts
@@ -8,3 +8,24 @@ export function cn(...inputs: ClassValue[]) {
 export function removeAccents(str: string): string {
   return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
+
+type QueryStringProps = {
+  name: string;
+  value: string;
+  searchParams: URLSearchParams;
+  queryToRemove?: string;
+};
+
+export function createQueryString({
+  name,
+  value,
+  searchParams,
+  queryToRemove,
+}: QueryStringProps) {
+  const params = new URLSearchParams(searchParams.toString());
+  params.set(name, value);
+  if (queryToRemove) {
+    params.delete(queryToRemove);
+  }
+  return params.toString();
+}
